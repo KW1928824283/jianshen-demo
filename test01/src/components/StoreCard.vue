ae3011e5ec1602a5d0ddb7d87ca0a39409381b2e
KerrWoo@qq.com <KerrWoo@qq.com>
2019-02-20 21:11:14 +0800

    添加底部推荐店家的卡片选项使用Slick插件

--- test01/src/components/StoreCard.vue
+++ test01/src/components/StoreCard.vue
@@ -1,74 +1,89 @@
 <template> 
-    <slick></slick>
+    <div>
+        
+        <slick class="card" ref="slick" :options="slickOptions">
+            <div class="card-item" v-for="item in data">
+                <div class="card-item-content">
+                    <img class="card-img" :src="item.src">
+                </div>
+                <span>{{item.name}}</span>
+                
+            </div>
+        </slick>
+    </div>
+    
 </template> 
 <script> 
   import '../assets/slick/slick.css'
   import '../assets/slick/slick-theme.css'
-  import 'swiper/dist/css/swiper.css'
   import Slick from 'vue-slick'
   import { swiper, swiperSlide } from 'vue-awesome-swiper'
   export default { 
     components: { 
-      swiper, 
-      swiperSlide,
       Slick 
     }, 
     data() { 
       return { 
         
-        carouselData:{},
+        data:[
+            {'src':'https://img.leoao.com/o_1bpg0u12q1tpc185h1lgh1fv3a022e.jpg?imageslim&imageView2/1/w/200/h/200',
+             'name':'嘉盛SOHO店'
+            },
+            {'src':'https://img.leoao.com/o_1bpi7qorv1panfne1eiv15aeu4h82.jpg?imageslim&imageView2/1/w/200/h/200',
+              'name':'嘉盛SOHO店'  
+            },
 
-        slidesToShow:2
-      } 
-    },
-    mounted:function(){
-        this.getCarouselData()
-    },
-    methods:{
-        getCarouselData:function(){
-            var Data =[
-                {
-                    'imgUrl':'https://img.leoao.com/FvX25IQgUIVNsp9UHtHQJ1CzbbgU?imageslim&imageView2/2/w/750/h/0',
-                    'isNav':true,
-                    'navUrl':'https://www.baidu.com'
-                },
-                {
-                    'imgUrl':'https://img.leoao.com/FlPwuZFTWFPvu-G280PVQX6G-r-N?imageslim&imageView2/2/w/750/h/0',
-                    'isNav':false
-                },
-                {
-
-                    'imgUrl':'https://img.leoao.com/Fk3Mzmdc8xqKRpWaaKzASqVhakoz?imageslim&imageView2/2/w/750/h/0',
-                    'isNav':true,
-                    'navUrl':'https://3.swiper.com.cn/api/Loop/2014/1215/22.html'
-                }
-            ]
-            this.carouselData = Data
-        },
-
-        navClick:function(index,event){
-
-            var data = this.carouselData
-            if(data[index].isNav){
-                console.log("jump to"+data[index].navUrl)
-                window.location.href = data[index].navUrl
+            {'src':'https://img.leoao.com/o_1ca7mqsl31g53da42obknperc16.jpg?imageslim&imageView2/1/w/200/h/200',
+            'name':'嘉盛SOHO店'
+            },
+            {'src':'https://img.leoao.com/o_1bpi60uuuovmqd11rna1nksitn3c.jpg?imageslim&imageView2/1/w/200/h/200',
+            'name':'嘉盛SOHO店'
+            },
+            {'src':'https://img.leoao.com/o_1bpg14rtnnqm1b7814gh1h3r1eon4u.jpg?imageslim&imageView2/1/w/200/h/200',
+            'name':'嘉盛SOHO店'
+            },
+            {'src':'https://img.leoao.com/o_1bvmekljv1aj4gea1p6011161g4od2.jpg?imageslim&imageView2/1/w/200/h/200',
+            'name':'嘉盛SOHO店'
             }
-        }
-    }  
+        ],
+
+        slickOptions: {
+            infinite: false,
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            accessibility:false,
+            arrows:false,
+            mobileFirst:true
+        },
+      } 
+    } 
   } 
 </script> 
 <style scoped> 
-    .container{
-        padding-top: 1rem;
-    }
+    
     img{
-        width: 100%;
-        height: auto;
+        width: 80%;
+        height:auto;
+        margin-left: 10%;
     }
+    
     .card{
         display: flex;
         flex-direction: row;
+    }
+    .card-item{
+        display: flex;
+        flex-direction: column;
+        align-items: center;  
+        
+    }
+    .card-img{
 
     }
-   
+    .card-item-content{
+
+        position: relative;
+        display: table-cell;
+        text-align: center;
+    }
 </style>
